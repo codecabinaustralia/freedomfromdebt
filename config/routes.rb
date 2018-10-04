608d53cb8230ff397b3a7296c98b79b73d6b9508
Josh Edgar <josh@blackhatdigital.com.au>
2018-10-04 11:31:18 +1000

    add nav

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'charge/view_downloads'
   get 'charge/charge_table'
   get 'static/home'
+  get 'static/detective'
 
   root to: "static#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
